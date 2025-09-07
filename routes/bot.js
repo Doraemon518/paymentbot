867df36e80526058d323262f83726070f92f1117
Doraemon518 <jeestuff33@gmail.com>
2025-09-07 17:57:11 +0530

    .

--- routes/bot.js
+++ routes/bot.js
@@ -2,11 +2,9 @@ const express = require('express')
 const router = express.Router()
 // define the home page route
 router.get('/', (req, res) => {
-  res.send('Birds home page')
+  console.log(req)
 })
 // define the about route
-router.get('/about', (req, res) => {
-  res.send('About birds')
-})
+
 
 module.exports = router
