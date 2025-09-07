d78b5b742395508d6683546fb199eea8e99638ba
Doraemon518 <jeestuff33@gmail.com>
2025-09-07 22:09:45 +0530

    .

--- routes/bot.js
+++ routes/bot.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const router = express.Router()
+let TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+Tele_token=process.env.bot_token;
+let bot=new TelegramBot(Tele_token)
+router.use(express.json())
+async function main(){
+    let webHookUrl=`${process.env.domain}/bot`
+    await bot.setWebHook(webHookUrl)
+}
+main()
+router.post('/bot',(req,res)=>{
+    console.log(req)
+})
+
 // define the home page route
 router.get('/', (req, res) => {
   console.log(req)
 })
 // define the about route
+
+
 
 
 module.exports = router
